229762bf633c4b6c8145bd397aa0d8d4578d5d5d
Caglar Turali <caglarturali@gmail.com>
2019-08-31 09:10:39 +0300

    Extract font selector into its own component

---
+++ src/components/FontSelector.svelte
@@ -0,0 +1,92 @@
+<script>
+  import {
+    fontFamily,
+    fonts,
+    isLoading,
+    isAllowedToCreateNew
+  } from "../stores.js";
+
+  export let label;
+
+  let newFont;
+  const addNewFontToList = () => {
+    if (!newFont || newFont == "") return;
+    fonts.update(f => [...f, newFont]);
+    fontFamily.set(newFont);
+    newFont = "";
+  };
+
+  const selectRandomFont = () => {
+    const randomFontIndex = Math.floor(Math.random() * $fonts.length);
+    fontFamily.set($fonts[randomFontIndex]);
+  };
+</script>
+
+<style>
+  .field-label {
+    flex-grow: 2;
+  }
+</style>
+
+<div class="field is-horizontal">
+  <div class="field-label is-normal">
+    <label class="label">{label}</label>
+  </div>
+  <div class="field-body">
+    <div class="field has-addons ">
+
+      <p class="control">
+        <span class="select is-fullwidth">
+          <select
+            bind:value={$fontFamily}
+            disabled={!$isAllowedToCreateNew || $isLoading}>
+            {#each $fonts as font}
+              <option value={font}>{font}</option>
+            {/each}
+          </select>
+        </span>
+      </p>
+      <p class="control">
+        <button
+          class="button"
+          on:click={() => selectRandomFont()}
+          class:is-static={!$isAllowedToCreateNew || $isLoading}>
+          <span class="icon">
+            <i class="fas fa-random" />
+          </span>
+        </button>
+      </p>
+
+    </div>
+  </div>
+</div>
+
+<div class="field is-horizontal">
+  <div class="field-label" />
+  <div class="field-body">
+    <div class="field is-expanded">
+      <div class="field has-addons">
+        <p class="control is-expanded">
+          <input
+            class="input"
+            type="text"
+            bind:value={newFont}
+            placeholder="Add new font to the list"
+            disabled={!$isAllowedToCreateNew || $isLoading} />
+        </p>
+        <p class="control">
+          <button
+            class="button"
+            on:click={() => addNewFontToList()}
+            class:is-static={!$isAllowedToCreateNew || $isLoading}>
+            <i class="fas fa-plus" />
+          </button>
+        </p>
+      </div>
+      <p class="help">
+        Make sure the new font is available on the page where the counter will
+        be placed
+      </p>
+    </div>
+  </div>
+</div>
--- src/containers/CustomizePanel.svelte
+++ src/containers/CustomizePanel.svelte
@@ -17,14 +17,10 @@
   import TileBox from "../components/TileBox.svelte";
   import ColorPicker from "../components/ColorPicker.svelte";
   import NumericInput from "../components/NumericInput.svelte";
+  import FontSelector from "../components/FontSelector.svelte";
 
   import { randomNumber, randomColor } from "../lib/utils.js";
   import { API_BASE } from "../shared.js";
-
-  const selectRandomFont = () => {
-    const randomFontIndex = Math.floor(Math.random() * $fonts.length);
-    fontFamily.set($fonts[randomFontIndex]);
-  };
 
   const handleRandomizeClick = () => {
     width.set(randomNumber(600));
@@ -33,15 +29,10 @@
     maxLength.set(randomNumber(13));
     backgroundColor.set(randomColor());
     fontColor.set(randomColor());
-    selectRandomFont();
-  };
 
-  let newFont;
-  const addNewFontToList = () => {
-    if (!newFont || newFont == "") return;
-    fonts.update(f => [...f, newFont]);
-    fontFamily.set(newFont);
-    newFont = "";
+    // Code duplication - rafactor
+    const randomFontIndex = Math.floor(Math.random() * $fonts.length);
+    fontFamily.set($fonts[randomFontIndex]);
   };
 
   const createCounter = async () => {
@@ -78,79 +69,14 @@
 </script>
 
 <style>
-  .field-label {
-    flex-grow: 2;
-  }
+
 </style>
 
 <TileBox title="Customize">
 
   <ColorPicker label="Background color" bind:value={$backgroundColor} />
   <ColorPicker label="Font color" bind:value={$fontColor} />
-
-  <div class="field is-horizontal">
-    <div class="field-label is-normal">
-      <label class="label">Font family</label>
-    </div>
-    <div class="field-body">
-      <div class="field has-addons ">
-
-        <p class="control">
-          <span class="select is-fullwidth">
-            <select
-              bind:value={$fontFamily}
-              disabled={!$isAllowedToCreateNew || $isLoading}>
-              {#each $fonts as font}
-                <option value={font}>{font}</option>
-              {/each}
-            </select>
-          </span>
-        </p>
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => selectRandomFont()}
-            class:is-static={!$isAllowedToCreateNew || $isLoading}>
-            <span class="icon">
-              <i class="fas fa-random" />
-            </span>
-          </button>
-        </p>
-
-      </div>
-    </div>
-  </div>
-
-  <div class="field is-horizontal">
-    <div class="field-label" />
-    <div class="field-body">
-      <div class="field is-expanded">
-        <div class="field has-addons">
-          <p class="control is-expanded">
-            <input
-              class="input"
-              type="text"
-              bind:value={newFont}
-              placeholder="Add new font to the list"
-              disabled={!$isAllowedToCreateNew || $isLoading} />
-          </p>
-          <p class="control">
-            <button
-              class="button"
-              on:click={() => addNewFontToList()}
-              class:is-static={!$isAllowedToCreateNew || $isLoading}>
-              <i class="fas fa-plus" />
-            </button>
-          </p>
-        </div>
-        <p class="help">
-          Make sure the new font is available on the page where the counter will
-          be placed
-        </p>
-      </div>
-    </div>
-  </div>
-
+  <FontSelector label="Font family" />
   <NumericInput label="Font size" max={50} bind:value={$fontSize} />
   <NumericInput label="Max digits" max={13} bind:value={$maxLength} />
   <NumericInput label="Width" max={600} bind:value={$width} />
