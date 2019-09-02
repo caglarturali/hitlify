18b7f46fe45647e36e60b6e3fc80f311077b0503
Caglar Turali <caglarturali@gmail.com>
2019-09-02 14:52:08 +0300

    Add Svelte logo to footer

--- src/layouts/components/Footer.svelte
+++ src/layouts/components/Footer.svelte
@@ -6,6 +6,17 @@
   .footer {
     padding: 2rem;
   }
+  .svelte {
+    display: flex;
+    justify-content: center;
+    width: 128px;
+    height: 36px;
+  }
+  .svelte img {
+    width: 90px;
+    height: auto;
+    padding-bottom: 4px;
+  }
 </style>
 
 <footer class="footer">
@@ -17,6 +28,16 @@
         <img
           src="https://ht-ct.now.sh/counters/4d169295-4c2c-4197-a1b3-a2904d0c1c75"
           alt="Total visitor count" />
+      </div>
+    </div>
+    <div class="level-item has-text-centered is-narrow">
+      <div>
+        <p class="heading">Built With</p>
+        <a href="https://svelte.dev/" target="_blank">
+          <div class="svelte">
+            <img src="https://svelte.dev/svelte-logotype.svg" alt="Svelte" />
+          </div>
+        </a>
       </div>
     </div>
     <div class="level-item has-text-centered is-narrow">
