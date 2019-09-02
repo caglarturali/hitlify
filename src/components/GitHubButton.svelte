5a6292ac819e43bd4cac3d051ce0b7bd98524e36
Caglar Turali <caglarturali@gmail.com>
2019-09-02 13:06:11 +0300

    Use localStorage to cache api response

--- src/components/GitHubButton.svelte
+++ src/components/GitHubButton.svelte
@@ -6,15 +6,34 @@
   let reposApiBase = "https://api.github.com/repos";
 
   let stars = 0;
+  let storageKey = `${user}-${repo}-json`;
 
   onMount(async () => {
+    if (!fetchLocalStorage()) {
+      await fetchApi();
+    }
+  });
+
+  const fetchLocalStorage = () => {
+    if (window.localStorage && localStorage.getItem(storageKey)) {
+      const jsonRecord = JSON.parse(localStorage.getItem(storageKey));
+      stars = jsonRecord.stargazers_count;
+      return true;
+    }
+    return false;
+  };
+
+  const fetchApi = async () => {
     const res = await fetch(`${reposApiBase}/${user}/${repo}`);
     const json = await res.json();
 
-    if (res.ok && json.stargazers_count) {
+    if (res.ok) {
       stars = json.stargazers_count;
+      if (window.localStorage) {
+        localStorage.setItem(storageKey, JSON.stringify(json));
+      }
     }
-  });
+  };
 </script>
 
 <style>
@@ -23,7 +42,7 @@
     border-radius: 2px;
     color: white;
     font-size: 0.8rem;
-    padding: 0 5px;
+    padding: 0 0;
     margin-left: 0.5em;
   }
 </style>
@@ -34,6 +53,9 @@
       <i class="fab fa-github-alt" />
     </span>
     <span>{repo}</span>
-    <span class="badge">{stars}</span>
+    <span class="badge">
+      {stars}
+      <i class="fas fa-star" />
+    </span>
   </a>
 </p>
